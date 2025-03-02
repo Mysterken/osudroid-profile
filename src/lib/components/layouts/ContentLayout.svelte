80bac10f402bd1b2ede1118cda26fec1c03fa866
Mysterken <Mysterkenzo@gmail.com>
2025-03-02 10:56:39 +0100

    edit wip page

---
+++ src/lib/components/layouts/ContentLayout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+
+<div
+	class="
+	bg-[#1E1E1E]
+	flex flex-col
+	phone-sm:gap-2.5
+	phone-lg:gap-3.5
+	tablet-sm:gap-5
+	tablet-sm:mx-5
+	tablet-lg:gap-6
+	tablet-lg:mx-6
+	desktop-sm:gap-8
+	desktop-sm:mx-8
+	desktop-lg:gap-8
+	desktop-lg:mx-8"
+>
+	<slot />
+</div>
--- src/routes/users/[uid]/+page.svelte
+++ src/routes/users/[uid]/+page.svelte
@@ -1,35 +1,44 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import type { ApiPlayer, ScraperPlayer } from '$lib/models/player';
+	import ContentLayout from '$lib/components/layouts/ContentLayout.svelte';
 
 	let user: ApiPlayer | ScraperPlayer | null = null;
 	let isLoading = true;
 
-	async function fetchUser(userId: string): Promise<ApiPlayer | ScraperPlayer | null> {
-		try {
-			const response = await fetch(`/api/users/${userId}`);
-			if (!response.ok) return null;
-			return (await response.json()) as ApiPlayer | ScraperPlayer;
-		} catch (error) {
-			console.error("Error fetching user:", error);
-			return null;
-		}
-	}
+	// async function fetchUser(userId: string): Promise<ApiPlayer | ScraperPlayer | null> {
+	// 	try {
+	// 		const response = await fetch(`/api/users/${userId}`);
+	// 		if (!response.ok) return null;
+	// 		return (await response.json()) as ApiPlayer | ScraperPlayer;
+	// 	} catch (error) {
+	// 		console.error("Error fetching user:", error);
+	// 		return null;
+	// 	}
+	// }
+	//
+	// onMount(async () => {
+	// 	const userId = window.location.pathname.split('/').pop() || "";
+	// 	user = await fetchUser(userId);
+	// 	isLoading = false;
+	// });
 
-	onMount(async () => {
-		const userId = window.location.pathname.split('/').pop() || "";
-		user = await fetchUser(userId);
-		isLoading = false;
-	});
+	user = {
+		Username: "John Doe",
+	};
+	isLoading = false;
 </script>
 
-
-{#if isLoading}
-	<p>Loading user data...</p>
-{:else}
-	{#if user}
-		<h1>Hello {user.Username}</h1>
+<ContentLayout>
+	{#if isLoading}
+		<p>Loading user data...</p>
 	{:else}
-		<p>User not found.</p>
+		{#if user}
+			<div style="background-color: #476426"><h1>Hello {user.Username}</h1></div>
+			<div style="background-color: #2077c4"><h1>card 2</h1></div>
+			<div style="background-color: #8018b2"><h1>card 3</h1></div>
+		{:else}
+			<p>User not found.</p>
+		{/if}
 	{/if}
-{/if}
+</ContentLayout>
