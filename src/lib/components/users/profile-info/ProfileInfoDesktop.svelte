003909c3766d60fc60961cd08f1850184e4b27a7
Mysterken <Mysterkenzo@gmail.com>
2025-03-12 20:08:25 +0100

    add content in ProfileInfoDesktop

--- src/lib/components/users/profile-info/ProfileInfoDesktop.svelte
+++ src/lib/components/users/profile-info/ProfileInfoDesktop.svelte
@@ -2,6 +2,12 @@
 import ContentCard from '$lib/components/layouts/ContentCard.svelte';
 </script>
 
-<ContentCard>
-	Profile Info Desktop
+<ContentCard sx="user-info flex gap-4">
+	<div class="user-avatar size-[80px] bg-[#FFFFFF] rounded-[10px] overflow-hidden">
+		<img src="https://via.placeholder.com/150" alt="User Avatar" />
+	</div>
+	<div class="user-info flex flex-col justify-end text-left">
+		<h1 class="font-bold text-lg">Username</h1>
+		<p class="text-sm text-gray-400">Country</p>
+	</div>
 </ContentCard>
