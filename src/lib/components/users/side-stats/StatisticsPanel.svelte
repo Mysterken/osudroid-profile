13ee1bcd951260821f9ba29a694660f33623b8d7
Mysterken <Mysterkenzo@gmail.com>
2025-03-15 11:29:48 +0100

    add statistics panel content

--- src/lib/components/users/side-stats/StatisticsPanel.svelte
+++ src/lib/components/users/side-stats/StatisticsPanel.svelte
@@ -1,7 +1,39 @@
 <script>
 	import ContentCard from '$lib/components/layouts/ContentCard.svelte';
+	import { ChartPieIcon } from 'lucide-svelte';
 </script>
 
-<ContentCard>
-	StatisticsPanel
+<ContentCard
+	sx="!p-2.5 flex flex-col gap-2.5"
+>
+	<div
+		class="
+			flex items-center gap-3.5"
+	>
+		<ChartPieIcon class="size-8" />
+		<h1 class="font-bold text-lg">Statistics</h1>
+	</div>
+
+	<div class="user-stats flex flex-col justify-center w-full px-2">
+		<table class="w-full text-sm max-w-[400px]">
+			<tbody>
+			<tr>
+				<td class="pb-1 text-left">Performance Points</td>
+				<td class="pb-1 text-right font-bold">6000</td>
+			</tr>
+			<tr>
+				<td class="pb-1 text-left">Score</td>
+				<td class="pb-1 text-right font-bold">17,183,008,068</td>
+			</tr>
+			<tr>
+				<td class="pb-1 text-left">Hit Accuracy</td>
+				<td class="pb-1 text-right font-bold">99.29%</td>
+			</tr>
+			<tr>
+				<td class="pb-1 text-left">Play Count</td>
+				<td class="pb-1 text-right font-bold">1213</td>
+			</tr>
+			</tbody>
+		</table>
+	</div>
 </ContentCard>
