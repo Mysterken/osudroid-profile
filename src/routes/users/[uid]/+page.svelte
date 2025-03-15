ece3070b0157ec64659f74adb722a7b71370a830
Mysterken <Mysterkenzo@gmail.com>
2025-03-15 15:50:26 +0100

    fix incorrect gap

--- src/routes/users/[uid]/+page.svelte
+++ src/routes/users/[uid]/+page.svelte
@@ -82,7 +82,7 @@
 				</div>
 
 				<!-- Main Content -->
-				<div class="flex flex-col gap-4">
+				<div class="flex flex-col gap-8">
 					<ProfileInfoDesktop />
 					<TopPlays />
 					<RecentPlays />
@@ -90,7 +90,15 @@
 			</div>
 
 			<!-- Mobile/Tablet Layout -->
-			<div class="desktop-sm:hidden flex flex-col gap-4">
+			<div
+				class="
+				desktop-sm:hidden
+				flex flex-col
+				phone-sm:gap-2.5 phone-sm:py-2.5
+				phone-lg:gap-3.5 phone-lg:py-3.5
+				tablet-sm:gap-5 tablet-sm:p-5
+				tablet-lg:gap-6 tablet-lg:p-6"
+			>
 				<ProfileInfoMobile
 					source={user.Source}
 					username={user.Username}
