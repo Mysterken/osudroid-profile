5820aa699a524f3876773ae3e7f158037963cf9a
Mysterken <Mysterkenzo@gmail.com>
2025-03-15 11:51:56 +0100

    add user info panel content

--- src/lib/components/users/side-stats/UserInfoPanel.svelte
+++ src/lib/components/users/side-stats/UserInfoPanel.svelte
@@ -1,7 +1,31 @@
 <script>
 	import ContentCard from '$lib/components/layouts/ContentCard.svelte';
+	import { InfoIcon } from 'lucide-svelte';
 </script>
 
-<ContentCard>
-	UserInfoPanel
+<ContentCard
+	sx="!p-2.5 flex flex-col gap-2.5"
+>
+	<div
+		class="
+			flex items-center gap-3.5"
+	>
+		<InfoIcon class="size-8" />
+		<h1 class="font-bold text-lg">User Info</h1>
+	</div>
+
+	<div class="user-stats flex flex-col justify-center w-full px-2">
+		<table class="w-full text-sm max-w-[400px]">
+			<tbody>
+			<tr>
+				<td class="pb-1 text-left">Registered</td>
+				<td class="pb-1 text-right font-bold">22/05/2016</td>
+			</tr>
+			<tr>
+				<td class="pb-1 text-left">Last Login</td>
+				<td class="pb-1 text-right font-bold">a year ago</td>
+			</tr>
+			</tbody>
+		</table>
+	</div>
 </ContentCard>
