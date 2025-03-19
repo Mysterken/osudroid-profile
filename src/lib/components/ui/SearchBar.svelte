1072b6bf065d7f030d69fe51d857ecb68b1d9ff0
Mysterken <Mysterkenzo@gmail.com>
2025-03-19 16:29:09 +0100

    fix searchbar not sticky

--- src/lib/components/ui/SearchBar.svelte
+++ src/lib/components/ui/SearchBar.svelte
@@ -5,7 +5,7 @@
 <nav
 	class="
 	bg-[#2A2A2A]
-	sticky
+	sticky top-0 z-50
 	flex flex-col
 	justify-center
 	p-2
@@ -19,12 +19,12 @@
 	h-16
 "
 >
-	<div class="max-w-[1200px] mx-auto size-full" >
+	<div class="max-w-[1200px] mx-auto size-full">
 		<div class="input-group grid-cols-[auto_1fr_auto] size-full max-w-3xl bg-[#565656] rounded-[10px]">
 			<div class="ig-cell preset-tonal">
 				<Search size={16} />
 			</div>
-			<input class="ig-input color-[red] shadow-inner" type="search" placeholder="Search player..." />
+			<input class="ig-input text-white shadow-inner" type="search" placeholder="Search player..." />
 		</div>
 	</div>
 </nav>
