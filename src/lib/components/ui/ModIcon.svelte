891b7b731749ee2c31dc8907ecde6b681fbd4af6
Mysterken <Mysterkenzo@gmail.com>
2025-03-21 11:09:59 +0100

    add multiplier mod icon

--- src/lib/components/ui/ModIcon.svelte
+++ src/lib/components/ui/ModIcon.svelte
@@ -16,3 +16,16 @@
 	<img class=" h-auto object-contain" src={imgSrc} alt={`${mod} mod icon`} style="max-width: {size}px"
 			 use:tooltip={{text: modName}} />
 {/if}
+
+{#if mod.startsWith('x')}
+	<div
+		class="
+		flex items-center justify-center
+		bg-[#1e1e1e]
+		border-[#3C3C3C] border-[1px] rounded-[5px]
+		text-white text-xs font-bold
+		px-2"
+	>
+		{mod}
+	</div>
+{/if}
