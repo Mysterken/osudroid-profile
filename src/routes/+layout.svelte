eec44da650ed19d1b189a80f9be74303a7d4d480
Mysterken <mysterken.dev@gmail.com>
2025-05-19 20:24:25 +0200

    update dependencies and change deprecated @inlang/paraglide-sveltekit into @inlang/paraglide-js

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import { i18n } from '$lib/i18n';
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	let { children } = $props();
 	import "../app.css";
 </script>
 
-<ParaglideJS {i18n}>
-	{@render children()}
-</ParaglideJS>
+{@render children()}
