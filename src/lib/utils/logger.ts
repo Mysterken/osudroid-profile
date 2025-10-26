bf6ab5b98c7fe55eea69a040959f64f3c6e4ac53
Mysterken <mysterken.dev@gmail.com>
2025-10-26 20:19:32 +0100

    fix pino-pretty in prod

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -3,12 +3,12 @@ import pino from 'pino';
 // Create logger instance
 const logger = pino({
 	level: process.env.LOG_LEVEL || 'info',
-	transport: {
-		target: 'pino-pretty',
-		options: {
-			colorize: true
+	...(process.env.NODE_ENV !== 'production' && {
+		transport: {
+			target: 'pino-pretty',
+			options: { colorize: true }
 		}
-	}
+	})
 });
 
 export default logger;
